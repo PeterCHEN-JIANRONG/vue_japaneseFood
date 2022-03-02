726c80f2ceaa572e8516d66760c18603010b97aa
peter.chen1024 <u0052017@gmail.com>
2022-03-02 23:36:38 +0800

    後台nav bar樣式

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,33 +1,38 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" to="/admin/products">阿波日本料理</router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/admin/products">產品列表</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">前台</router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" @click.prevent="logout">登出</a>
-          </li>
-        </ul>
-      </div>
+  <div class="bg-dark">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <router-link class="navbar-brand" to="/admin/products">阿波日本料理</router-link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/admin/products">產品列表</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/">前台</router-link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" @click.prevent="logout">登出</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </nav>
+  </div>
+
   <router-view v-if="loginCheck" />
 </template>
 
