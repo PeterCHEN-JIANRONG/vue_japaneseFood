fa22aa59a8053567f0da47d216e673f5c243fa79
peter.chen1024 <u0052017@gmail.com>
2022-03-10 23:03:47 +0800

    add couponModal

--- src/views/Dashboard/AdminCoupons.vue
+++ src/views/Dashboard/AdminCoupons.vue
@@ -21,11 +21,18 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, key) in coupons" :key="`${key}${item.id}`">
+        <tr
+          v-for="(item, key) in coupons"
+          :key="`${key}${item.id}`"
+          :class="{ 'text-secondary': !item.is_enabled }"
+        >
           <td>{{ item.title }}</td>
           <td>{{ item.code }}</td>
           <td>{{ item.percent }}%</td>
-          <td>{{ $filters.date(item.due_date) }}</td>
+          <td>
+            {{ $filters.date(item.due_date) }}
+            <span v-if="item.due_date < new Date() / 1000">(已過期)</span>
+          </td>
           <td>
             <span v-if="item.is_enabled === 1" class="text-success">啟用</span>
             <span v-else class="text-muted">未啟用</span>
@@ -54,6 +61,12 @@
     <div class="text-end">共 {{ coupons.length }} 個</div>
     <Pagination :pagination="pagination" @get-products="getCoupons"></Pagination>
   </div>
+  <AdminCouponModal
+    ref="couponModal"
+    :coupon="tempCoupon"
+    :isNew="isNew"
+    @update-coupon="updateCoupon"
+  ></AdminCouponModal>
   <DelModal
     ref="delModal"
     :item="tempCoupon"
@@ -65,6 +78,7 @@
 
 <script>
 import Pagination from '@/components/PaginationView.vue';
+import AdminCouponModal from '@/components/AdminCouponModal.vue';
 import DelModal from '@/components/DelModal.vue';
 
 export default {
@@ -76,10 +90,12 @@ export default {
       isLoadingItem: '',
       pagination: {},
       currentPage: 1,
+      isNew: false,
     };
   },
   components: {
     Pagination,
+    AdminCouponModal,
     DelModal,
   },
   methods: {
@@ -99,6 +115,32 @@ export default {
           this.$httpMessageState(err.response, '錯誤訊息');
         });
     },
+    updateCoupon(item) {
+      this.isLoadingItem = item.id;
+      let url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/coupon`;
+      let httpMethos = 'post';
+      const data = item;
+
+      if (!this.isNew) {
+        url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/coupon/${this.tempCoupon.id}`;
+        httpMethos = 'put';
+      }
+      this.$http[httpMethos](url, { data })
+        .then((res) => {
+          this.isLoadingItem = '';
+          this.$refs.couponModal.hideModal();
+          this.$httpMessageState(res, '新增優惠券');
+          if (this.isNew) {
+            this.getCoupons(); // 新增:回第一頁
+          } else {
+            this.getCoupons(this.currentPage);
+          }
+        })
+        .catch((error) => {
+          this.isLoadingItem = '';
+          this.$httpMessageState(error.response, '錯誤訊息');
+        });
+    },
     deleteCoupon() {
       this.isLoadingItem = this.tempCoupon.id;
       const url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/coupon/${this.tempCoupon.id}`;
@@ -114,6 +156,21 @@ export default {
           this.isLoadingItem = '';
           this.$httpMessageState(err.response, '錯誤訊息');
         });
+    },
+    openCouponModal(isNew, item) {
+      this.isNew = isNew;
+      if (this.isNew) {
+        this.tempCoupon = {
+          due_date: new Date().getTime() / 1000,
+          is_enabled: 0,
+          percent: 90,
+          title: '週年慶',
+          code: 'A00',
+        };
+      } else {
+        this.tempCoupon = { ...item };
+      }
+      this.$refs.couponModal.openModal();
     },
     openDelCouponModal(item) {
       this.tempCoupon = { ...item };
